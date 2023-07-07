c67c14fd140e010605ea881996062a1736305c5a
chris_beltran <cristianb72@hotmail.com>
2023-07-07 16:19:29 -0600

    items side bar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -19,45 +19,71 @@ return(
             <div className="sidebarWrapper">
                 <ul className="sidebarList">
                     <li className="sidebarListItem">
-                        <RssFeed className='sidebarIcon'/>
+                         <img src="src/assets/1f3e0.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
                         <span className="sidebarListItemText">
-                            Feed
+                            Home
                         </span>
                     </li>
                     <li className="sidebarListItem">
-            <Chat className="sidebarIcon" />
+             <img src="/src/assets/1f6cd.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
             <span className="sidebarListItemText">Chats</span>
           </li>
           <li className="sidebarListItem">
-            <PlayCircleFilledOutlined className="sidebarIcon" />
+            <img src="/src/assets/1f4dc.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
             <span className="sidebarListItemText">Videos</span>
           </li>
           <li className="sidebarListItem">
-            <Group className="sidebarIcon" />
+           <img src="/src/assets/2764.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
             <span className="sidebarListItemText">Groups</span>
           </li>
           <li className="sidebarListItem">
-            <Bookmark className="sidebarIcon" />
+          <img src="/src/assets/1f914.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
             <span className="sidebarListItemText">Bookmarks</span>
           </li>
           <li className="sidebarListItem">
-            <HelpOutline className="sidebarIcon" />
+          <img src="/src/assets/1f399.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
             <span className="sidebarListItemText">Questions</span>
           </li>
           <li className="sidebarListItem">
-            <WorkOutline className="sidebarIcon" />
+          <img src="/src/assets/1f3f7.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
             <span className="sidebarListItemText">Jobs</span>
           </li>
           <li className="sidebarListItem">
-            <Event className="sidebarIcon" />
+          <img src="/src/assets/1f4a1.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
             <span className="sidebarListItemText">Events</span>
           </li>
           <li className="sidebarListItem">
-            <School className="sidebarIcon" />
+          <img src="/src/assets/1f3f7.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
             <span className="sidebarListItemText">Courses</span>
           </li>
                 </ul>
-                <button className="sidebarButton">Show More</button>
+          <button className="sidebarButton">Show More</button>
+          <br></br>
+                <li className="sidebarListItem">
+          <img src="/src/assets/1f44d.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
+            <span className="sidebarListItemText">Code of conduct</span>
+          </li>
+          <li className="sidebarListItem">
+          <img src="/src/assets/1f913.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
+            <span className="sidebarListItemText">Privacy Policy</span>
+          </li>
+          <li className="sidebarListItem">
+          <img src="/src/assets/1f440.svg" alt="readin list" 
+                                    class="icono-home" width="24px" height="24px"/>
+            <span className="sidebarListItemText">Terms of Use</span>
+          </li>
+
         <hr className="sidebarHr" />
         <ul className="sidebarFriendList">
         {Users.map((u) => (
